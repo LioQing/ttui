eab4b35dbda8899120968641408d59c4dcd4b01b
LioQing <46854695+LioQing@users.noreply.github.com>
2022-03-30 15:14:25 +0800

    Add docs
    btw last commit message is supposed to be ttui:\:mouse::EnableEvent

--- include/ttui/core/mouse.hpp
+++ include/ttui/core/mouse.hpp
@@ -4,5 +4,10 @@
 
 namespace ttui { namespace mouse
 {
+    /**
+     * @brief Enable or disable mouse input events.
+     * 
+     * @param enable Boolean indicating whether enable or disable
+     */
     void EnableEvent(bool enable);
 }}
--- include/ttui/core/screen.hpp
+++ include/ttui/core/screen.hpp
@@ -7,7 +7,7 @@ namespace ttui { namespace screen
     /**
      * @brief Clear the screen, cursor position will be (0, 0) after this. Note using this with Handle::Draw every frame will cause flickering, use Empty widget instead.
      * 
-     * @param color 
+     * @param color The color to clear the screen with
      */
     void Clear(const Color& color = Color::Reset());
 }}
